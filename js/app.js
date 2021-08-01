266c53ea390c9effbb1cab4577b4cf2e16d14eb3
Arbaz Khan <arbazkhangul123@gmail.com>
2021-08-01 13:36:04 -0700

    toxx

--- js/app.js
+++ js/app.js
@@ -11,12 +11,17 @@ window.addEventListener("beforeinstallprompt", (e) => {
 const installApp = document.getElementById("installApp");
 
 installApp.addEventListener("click", async () => {
+  console.log(deferredPrompt);
   if (deferredPrompt !== null) {
     deferredPrompt.prompt();
     const { outcome } = await deferredPrompt.userChoice;
     if (outcome === "accepted") {
       deferredPrompt = null;
     }
+  } else {
+    alert(
+      "This is not working in your browser you can download this app simply by clicking on add to home screen"
+    );
   }
 });
 
