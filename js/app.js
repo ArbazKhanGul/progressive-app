310c23a0cbf362a336e554af3c7ee3c443753db3
Arbaz Khan <arbazkhangul123@gmail.com>
2021-08-01 11:57:18 -0700

    tokjxfffddfff

--- js/app.js
+++ js/app.js
@@ -1,5 +1,24 @@
-if('serviceWorker' in navigator){
-  navigator.serviceWorker.register('/sw.js')
-    .then(reg => console.log('service worker registered', reg))
-    .catch(err => console.log('service worker not registered', err));
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("/sw.js")
+    .then((reg) => console.log("service worker registered", reg))
+    .catch((err) => console.log("service worker not registered", err));
 }
+
+let deferredPrompt;
+
+window.addEventListener("beforeinstallprompt", (e) => {
+  deferredPrompt = e;
+});
+
+const installApp = document.getElementById("installApp");
+
+installApp.addEventListener("click", async () => {
+  if (deferredPrompt !== null) {
+    deferredPrompt.prompt();
+    const { outcome } = await deferredPrompt.userChoice;
+    if (outcome === "accepted") {
+      deferredPrompt = null;
+    }
+  }
+});
