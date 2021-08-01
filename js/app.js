4ac5cba3307ea7ea0afa46252b4758ecb55648ef
Arbaz Khan <arbazkhangul123@gmail.com>
2021-08-01 12:35:53 -0700

    toxxcfgdgfx

--- js/app.js
+++ js/app.js
@@ -1,20 +1,4 @@
-let deferredPrompt;
 
-window.addEventListener("beforeinstallprompt", (e) => {
-  deferredPrompt = e;
-});
-
-const installApp = document.getElementById("installApp");
-
-installApp.addEventListener("click", async () => {
-  if (deferredPrompt !== null) {
-    deferredPrompt.prompt();
-    const { outcome } = await deferredPrompt.userChoice;
-    if (outcome === "accepted") {
-      deferredPrompt = null;
-    }
-  }
-});
 
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker
