ca37d2bc548d1674a861c180974a14601b102dbb
Arbaz Khan <arbazkhangul123@gmail.com>
2021-08-01 13:17:56 -0700

    toxxcfgdgfxfffgf

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ window.addEventListener("beforeinstallprompt", (e) => {
   // Stash the event so it can be triggered later.
   deferredPrompt = e;
   // Update UI notify the user they can install the PWA
-  showInstallPromotion();
+  // showInstallPromotion();
 });
 const installApp = document.getElementById("installApp");
 
